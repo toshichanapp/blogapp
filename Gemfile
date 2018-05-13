f6c17aa90ab7533e38122b72de5b3bdb28cb4f71
fuzisan <toshichanapp@gmail.com>
2018-05-13 14:07:28 +0000

    letter_opener_web 導入

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem "rspec-rails"
+  gem 'letter_opener_web'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :blogs
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
